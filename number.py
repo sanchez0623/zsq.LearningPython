d352e943e29a34f4504b03f31a08c36e3ce61d71
sanchez0623 <18576645357@163.com>
2019-03-10 23:51:34 +0800

    number demo

---
+++ number.py
@@ -0,0 +1,21 @@
+# 1.整形 int
+# 其他语言有short，int，long
+
+# 2.浮点数 float
+# 其他语言有float，double
+# py的float相当于其他语言的double
+
+print(type(1));
+print(type(-1));
+print(type(1.1))
+
+print(type(1+1.1));
+print(type(1+1.0));
+
+print(type(1*1));
+print(type(1*1.0));
+
+# 输出float
+print(type(2/2));
+# 输出int————'//'表示整除
+print(type(2//2));

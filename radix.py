fa3d9a84ac70f08cdefe9d069980313c7f5d46b9
sanchez0623 <18576645357@163.com>
2019-03-11 00:02:53 +0800

    radix function in py

---
+++ radix.py
@@ -0,0 +1,32 @@
+# 进制
+
+# 二进制
+# 使用0b开头 b表示binary
+print(0b10);
+
+#八进制
+# 使用0o开头 o表示octonary
+print(0o10);
+
+#十六进制
+# 使用0x开头 x表示hex
+print(0x10);
+
+#进制转换
+#用py内置函数bin,int,hex
+
+print(bin(10));
+print(bin(0b10));
+print(bin(0x10));
+
+print(int(0b10));
+print(int(0o10));
+print(int(0x10));
+
+print(hex(0b10));
+print(hex(0o10));
+print(hex(10));
+
+print(oct(0b10));
+print(oct(10));
+print(oct(0x10));

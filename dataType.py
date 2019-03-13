ba9dbd0da7664a979c824f670188b8106d1f4d12
sanchez0623 <18576645357@163.com>
2019-03-13 22:40:52 +0800

    add 复数

--- dataType.py
+++ dataType.py
@@ -1,8 +1,8 @@
 # number:数字
+
 # bool ：首字符大写，——数字类型的一种
 #       True 非0，非空字符串，数组，元组
 #       False 0 ，空字符串，空数组，空元组
-# complex:复数
 
 print(type(True));
 print(type(False));
@@ -20,3 +20,7 @@ print(bool({1}));
 print(bool({}));
 
 print(bool(None));
+
+
+# complex:复数
+print(36j);

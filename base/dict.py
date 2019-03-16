2c98ebb7e6c2309f219372765200b2fb3b8c55d4
sanchez0623 <18576645357@163.com>
2019-03-16 20:11:57 +0800

    dict demo

---
+++ base/dict.py
@@ -0,0 +1,17 @@
+# dict——字典——{key：value}——无序
+# key-value类型
+# key必须是不可变的类型
+
+# 不支持 下标、切片
+
+print(type({1:1,2:2,3:3}));
+print(type({'1':1,1:1,2:2,3:3}));
+
+print({1:1,2:2,3:3});
+print({'1':1,1:1,2:2,3:3});
+
+print({[1,2]:1,2:2,3:3});#异常：TypeError: unhashable type: 'list'
+print({(1,2):1,2:2,3:3});#使用tuple作为key，正常
+
+# 如何定义空字典？
+print(type({}));

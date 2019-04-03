0f6ad08abc28736229220cab41aa84029408693f
sanchez.zou <sanchez.zou@hirede.com>
2019-04-03 20:11:10 +0800

    1.类介绍 2.构造函数 3.方法

---
+++ oop/c1.py
@@ -0,0 +1,40 @@
+# 类
+# 面向对象
+# 类和对象
+
+
+class Student():
+    name = 'demo'
+    age = 18
+
+    # 构造函数
+    # 1.不返回，或者只能返回None类型——return None
+    # 2.self表示当前实例
+    def __init__(self, name, age):
+        # 错误方式：给'类变量'赋值
+        # name = name
+        # age = age
+        # 正确：给'实例变量'赋值
+        self.name = name
+        self.age = age
+
+    # 定义方法需要加默认参数self
+    def print_file(self):
+        # 调用类变量时，要加self.xxx
+        print(self.name)
+        print(str(self.age))
+
+
+# student = Student()
+# student.print_file()
+
+# student = Student()
+# a = student.__init__()
+# print(a)
+# print(type(a))
+
+student = Student('zsq', 18)
+print(student.name)
+student2 = Student('sanchez', 19)
+print(student2.name)
+print(Student.name)
